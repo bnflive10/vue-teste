344a02be4d77c497db633aa45f34afb7f3356ba6
Bernardino Das Neves <bnflive10@gmail.com>
2022-11-10 16:21:17 +0200

    link com v-bind

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
-        <p>Utiliso as seguintes tecnologias</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Utiliso as seguintes tecnologias</p>
         <ul>
             <li>Javascript</li>
             <li>PHP</li>
             <li>Phyton</li>
         </ul>
+        <p>Para acessar o nosso portifolio <a v-bind:href="meu_link">Aqui!</a></p>
     </div>
 </template>
 
 <script>
     export default {
         name: "info",
+        data(){
+            return {
+                esta_trabalhando: true,
+                meu_link: 'https://www.google.com'
+            }
+        }
     }
 </script>
