37a3332429d947afc7b8b930e26ad278e94900df
Bernardino Das Neves <bnflive10@gmail.com>
2022-11-10 16:39:09 +0200

    picture v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
-  <div class="container">
-  <h1>Ola Mundo!</h1>
-  <PrimeiroComponente/>
-  <LifeCycle/>
-  <pessoa/>
-</div>
+  <div class="container-fluid">
+    <div class="row">
+
+      <div class="col-2">
+        <Picture/>
+      </div>
+    </div>
+    <h1>Ola Mundo!</h1>
+    <PrimeiroComponente />
+    <LifeCycle />
+    <pessoa />
+  </div>
 </template>
 
 <script>
-import PrimeiroComponente from './components/PrimeiroComponente.vue';
-import LifeCycle from './components/LifeCycle.vue';
-import Pessoa from './components/Pessoa.vue';
-
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
+import Pessoa from "./components/Pessoa.vue";
+import Picture from "./components/Picture.vue";
 
 export default {
-  name: 'App',
-  components:{
+  name: "App",
+  components: {
     PrimeiroComponente,
     LifeCycle,
-    Pessoa
-  }
-}
+    Pessoa,
+    Picture,
+  },
+};
 </script>
---
+++ src/components/Picture.vue
@@ -0,0 +1,14 @@
+<template>
+    <img :src="logo" alt="">
+</template>
+
+<script>
+    export default {
+        name: "Picture",
+        data(){
+            return{
+                logo: '/img/logo-nagex.png'
+            }
+        }
+    }
+</script>
